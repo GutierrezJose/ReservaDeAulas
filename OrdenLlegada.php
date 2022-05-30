fbdf18f4643380d09bab69967f9f493aa95e826b
andres201933 <andres201933@gmail.com>
2022-05-30 07:48:22 -0400

    2do intento de push interfaz de admin

--- OrdenLlegada.php
+++ OrdenLlegada.php
@@ -33,16 +33,25 @@ $server = "localhost";
             <ul class="lisst-unstyled components">
             <li class="active">
                 <li>
-                    <a href="OrdenLlegada.php" class="d-block  p-3"><i class="icon ion-md-person mr-2 lead"></i> Reservas por orden de llegada</a>
+                    <h1><i class="icon ion-md-person mr-2 lead"> Administrador</i></h1>
                 </li>
                 <li>
-                    <a href="FechaProxima.php" class="d-block  p-3"><i class="icon ion-md-person mr-2 lead"></i> Reservas por fecha proxima</a>
+                    <a href="OrdenLlegada.php" class="d-block  p-3"><i class="icon ion-md-document mr-2 lead"></i> Reservas por orden de llegada</a>
                 </li>
                 <li>
-                    <a href="Urgencia.php" class="d-block  p-3"><i class="icon ion-md-person mr-2 lead"></i> Reservas por urgencia</a>
+                    <a href="FechaProxima.php" class="d-block  p-3"><i class="icon ion-md-calendar mr-2 lead"></i> Reservas por fecha proxima</a>
                 </li>
                 <li>
-                    <a href="MAXmin.html" class="d-block  p-3"><i class="icon ion-md-person mr-2 lead"></i> Cambiar limite de reservas</a>
+                    <a href="Urgencia.php" class="d-block  p-3"><i class="icon ion-md-alert mr-2 lead"></i> Reservas por urgencia</a>
+                </li>
+                <li>
+                    <a href="MAXmin.html" class="d-block  p-3"><i class="icon ion-md-create mr-2 lead"></i> Cambiar limite de reservas</a>
+                </li>
+                <li>
+                    <a href="AñadirAula.html" class="d-block  p-3"><i class="icon ion-md-add mr-2 lead"></i> Añadir Aulas</a>
+                </li>
+                <li>
+                    <a href="ListaDeAulas.html" class="d-block  p-3"><i class="icon ion-md-document mr-2 lead"></i> Lista de aulas</a>
                 </li>
             </li>
         </nav>      
